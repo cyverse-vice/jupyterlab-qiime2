e7b708bb1ef19a5d9a7f2b7e48c8b78e5557c621
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2022-12-01 18:10:41 +0000

    switched .bashrc to .bash_profile for interactive -it change, now it works

--- 2022.8/entry.sh
+++ 2022.8/entry.sh
@@ -2,6 +2,4 @@
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
-source /home/qiime2/.bashrc
-
 exec jupyter lab --no-browser --ip=0.0.0.0 --config=/opt/conda/envs/qiime2-2022.8/etc/jupyter/jupyter_notebook_config.json --ServerApp.token="" --SeverApp.password=""
